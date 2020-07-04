3493366f016fb63b8cc4bae0d47451f036c13070
pini85 <40143951+pini85@users.noreply.github.com>
2020-07-04 16:18:36 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -7,8 +7,3 @@ if(process.env.NODE_ENV === "production"){
   //in development
   module.exports = require("./dev");
 }
-BcN4EWq84YnV06sK;
-mongodb+srv://pini1985:<password>@cluster0.ecuqo.mongodb.net/<dbname>?retryWrites=true&w=majority
-
-clinetid:"139420694085-0061465vv1rphfskcrqtn671gk3628jl.apps.googleusercontent.com"
-aecretid="d0iJDc79PZ6LyytHlzR2logH"
