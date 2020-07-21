d0c754aa15f217c15fc1a0f246d6efa0547f7d64
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2020-07-21 17:33:51 +0300

    a

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,10 +8,21 @@ module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
-    const survey = (new Survey() = new Survey({
+    const survey = new Survey({
       title,
       subject,
       body,
-    }));
+      //we will get an array of comma seperated emails. So here we split it and put it the email in an object and we trim the email just in case there are accident white spaces in it.
+      recipients: recipients
+        .split(',')
+        .map((email) => ({ email: email.trim() })),
+      //mongo and mongoose provides us with the user id.
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
   });
 };
+
+/*
+When we pass in an array of objects mongoose will automatically create that sub document collection for us for the recipients.
+*/
